63f0c15e8e6841e2b5a342cffff8b12c32af59f3
GaganGowdaCS <gowdagagan02@gmail.com>
2023-11-20 14:07:47 -0600

    new branch initial commit

--- socketProgramming/src/socketProgramming/TCPServerEx.java
+++ socketProgramming/src/socketProgramming/TCPServerEx.java
@@ -5,30 +5,23 @@ import java.net.*;
 
 class TCPServer { 
 
-  public static void main(String argv[]) throws Exception 
-    { 
-      String clientSentence; 
-      String capitalizedSentence; 
+	public static void main(String argv[]) throws Exception { 
+		String clientSentence; 
+		String capitalizedSentence; 
 
-      ServerSocket welcomeSocket = new ServerSocket(6789); 
+		ServerSocket welcomeSocket = new ServerSocket(6789); 
   
-      while(true) { 
+		while(true) { 
   
-            Socket connectionSocket = welcomeSocket.accept(); 
+			Socket connectionSocket = welcomeSocket.accept(); 
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
+			DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream()); 
 
-           BufferedReader inFromClient = 
-              new BufferedReader(new
-              InputStreamReader(connectionSocket.getInputStream())); 
+			clientSentence = inFromClient.readLine(); 
 
+			capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
 
-           DataOutputStream  outToClient = 
-             new DataOutputStream(connectionSocket.getOutputStream()); 
-
-           clientSentence = inFromClient.readLine(); 
-
-           capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
-
-           outToClient.writeBytes(capitalizedSentence); 
+			outToClient.writeBytes(capitalizedSentence); 
         } 
     } 
 } 
