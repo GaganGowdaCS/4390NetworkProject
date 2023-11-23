17d6503f47c6eec339abf2b51bb2d06af52bcb5e
GaganGowdaCS <gowdagagan02@gmail.com>
2023-11-22 22:14:21 -0600

    first version of working code

--- socketProgramming/src/projectCode/Client.java
+++ socketProgramming/src/projectCode/Client.java
@@ -1,5 +1,58 @@
 package projectCode;
 
-public class Client {
+import java.io.*;
+import java.net.*;
+import java.util.Random;
 
+public class Client {
+    private static final String SERVER_IP = "127.0.0.1"; // Change this to server IP address
+    private static final int PORT = 5000;
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket(SERVER_IP, PORT);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
+
+            // Initial attachment - Providing username
+            System.out.println("Enter your username:");
+            String username = consoleInput.readLine();
+            out.println(username);
+
+            // Wait for server acknowledgement
+            String serverResponse = in.readLine();
+            System.out.println("Server response: " + serverResponse);
+
+            //Random random = new Random();
+            
+            //Prompt the user 3 times
+            for (int i = 0; i < 3; i++) {
+                // Send random math calculation requests
+                //int num1 = random.nextInt(10);
+                //int num2 = random.nextInt(10);
+                //String request = num1 + " + " + num2; // Example request: Addition
+                //out.println(request);
+            	
+            	//Record user input and send to server application
+            	System.out.println("Enter math equation: (num + num)");
+                String equation = consoleInput.readLine();
+                out.println(equation);
+
+                // Wait for server response
+                serverResponse = in.readLine();
+                System.out.println("Server response: " + serverResponse);
+
+                // Simulate random time delay between requests
+                Thread.sleep(1000);
+            }
+
+            // Close connection request
+            out.println("CLOSE_CONNECTION");
+            System.out.println("Connection closed.");
+            socket.close();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- socketProgramming/src/projectCode/Server.java
+++ socketProgramming/src/projectCode/Server.java
@@ -1,5 +1,105 @@
 package projectCode;
 
-public class Server {
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
+public class Server {
+    private static final int PORT = 5000;
+    private static Map<String, Date> users = new HashMap<>();
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Math server is running...");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected: " + socket);
+
+                // Start a new thread to handle client's requests
+                ClientHandler clientHandler = new ClientHandler(socket);
+                new Thread(clientHandler).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class ClientHandler implements Runnable {
+        private Socket socket;
+        private PrintWriter out;
+        private BufferedReader in;
+        private String username;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            try {
+                out = new PrintWriter(socket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                // Get client's username during initial attachment
+                //out.println("Enter your username:");
+                username = in.readLine();
+                users.put(username, new Date());
+                out.println("Connection successful. Welcome, " + username + "!");
+
+                String request;
+                while ((request = in.readLine()) != null) {
+                    System.out.println("Request from " + username + ": " + request);
+                    // Process the request - Basic math calculations
+                    String response = processRequest(request);
+                    //out.println("Response from server: " + response);
+                    out.println(response);
+                }
+            } catch (IOException e) {
+                System.out.println("Connection closed with client: " + username);
+                users.remove(username);
+            }
+        }
+
+        private String processRequest(String request) {
+            // Perform basic math calculations
+            // Addition, Subtraction, Multiplication, Division
+        	// Split the request into operands and operator
+            String[] parts = request.split("\\s+");
+            if (parts.length != 3) {
+                return "Invalid request format. Please use 'operand operator operand'.";
+            }
+
+            try {
+                int operand1 = Integer.parseInt(parts[0]);
+                String operator = parts[1];
+                int operand2 = Integer.parseInt(parts[2]);
+
+                int result;
+                switch (operator) {
+                    case "+":
+                        result = operand1 + operand2;
+                        break;
+                    case "-":
+                        result = operand1 - operand2;
+                        break;
+                    case "*":
+                        result = operand1 * operand2;
+                        break;
+                    case "/":
+                        if (operand2 == 0) {
+                            return "Cannot divide by zero.";
+                        }
+                        result = operand1 / operand2;
+                        break;
+                    default:
+                        return "Invalid operator. Please use '+', '-', '*', or '/'.";
+                }
+                return String.valueOf(result);
+            } catch (NumberFormatException e) {
+                return "Invalid operands. Please use numbers for operands.";
+            }
+            //return "Processed: " + request;
+        }
+    }
 }
