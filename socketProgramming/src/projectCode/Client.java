0c624979a01c8c6a8f77b6ef08cf64c190d84778
GaganGowdaCS <gowdagagan02@gmail.com>
2023-11-29 20:49:13 -0600

    added comments, users can now enter more than 3 equations

--- socketProgramming/src/projectCode/Client.java
+++ socketProgramming/src/projectCode/Client.java
@@ -5,49 +5,51 @@ import java.net.*;
 import java.util.Random;
 
 public class Client {
-    private static final String SERVER_IP = "127.0.0.1"; // Change this to server IP address
+	// Initialize variables
+    private static final String SERVER_IP = "127.0.0.1"; 
     private static final int PORT = 5000;
 
     public static void main(String[] args) {
         try {
+        	// Create TCP socket and input/output
             Socket socket = new Socket(SERVER_IP, PORT);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
 
-            // Initial attachment - Providing username
+            // Prompt username from Client
             System.out.println("Enter your username:");
             String username = consoleInput.readLine();
             out.println(username);
 
-            // Wait for server acknowledgement
+            // Receive acknowledgement from server
             String serverResponse = in.readLine();
             System.out.println("Server response: " + serverResponse);
 
-            //Random random = new Random();
-            
-            //Prompt the user 3 times
-            for (int i = 0; i < 3; i++) {
-                // Send random math calculation requests
-                //int num1 = random.nextInt(10);
-                //int num2 = random.nextInt(10);
-                //String request = num1 + " + " + num2; // Example request: Addition
-                //out.println(request);
-            	
-            	//Record user input and send to server application
-            	System.out.println("Enter math equation: (num + num)");
-                String equation = consoleInput.readLine();
+            // Begin prompting user for basic math equations
+            String equation = "";
+            while (!equation.equals("done")){
+
+            	//Record user input and send to server 
+            	System.out.println("Enter math equation: (num + num). To end, type 'done'");
+                equation = consoleInput.readLine();
+                
+                // If user enters "done" then client program is stopped
+                if (equation.equals("done")) {
+                	break;
+                }
+                // Send user input to server
                 out.println(equation);
 
                 // Wait for server response
                 serverResponse = in.readLine();
                 System.out.println("Server response: " + serverResponse);
 
-                // Simulate random time delay between requests
+                // Add time delay between requests
                 Thread.sleep(1000);
             }
 
-            // Close connection request
+            // Close connection request after 3 equations
             out.println("CLOSE_CONNECTION");
             System.out.println("Connection closed.");
             socket.close();
--- socketProgramming/src/projectCode/Server.java
+++ socketProgramming/src/projectCode/Server.java
@@ -5,14 +5,17 @@ import java.net.*;
 import java.util.*;
 
 public class Server {
+	// Initialize variables
     private static final int PORT = 5000;
     private static Map<String, Date> users = new HashMap<>();
 
     public static void main(String[] args) {
         try {
+        	// Create TCP server socket
             ServerSocket serverSocket = new ServerSocket(PORT);
             System.out.println("Math server is running...");
 
+            // Keep accepting new users while server is online
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("New client connected: " + socket);
@@ -27,6 +30,7 @@ public class Server {
     }
 
     static class ClientHandler implements Runnable {
+    	// Initialize variables
         private Socket socket;
         private PrintWriter out;
         private BufferedReader in;
@@ -38,43 +42,50 @@ public class Server {
 
         public void run() {
             try {
+            	// Initialize input and output
                 out = new PrintWriter(socket.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                // Get client's username during initial attachment
-                //out.println("Enter your username:");
+                // Get username from client program
                 username = in.readLine();
+                // Add users to map
                 users.put(username, new Date());
+                // Confirm connection to specific user
                 out.println("Connection successful. Welcome, " + username + "!");
 
+                // Process request and send response
                 String request;
                 while ((request = in.readLine()) != null) {
                     System.out.println("Request from " + username + ": " + request);
-                    // Process the request - Basic math calculations
+                    // Process basic math calculation
                     String response = processRequest(request);
-                    //out.println("Response from server: " + response);
+                    // Send response
                     out.println(response);
                 }
             } catch (IOException e) {
+            	// When client disconnects, remove them from active users
                 System.out.println("Connection closed with client: " + username);
                 users.remove(username);
             }
         }
 
+        // Method for processing client request 
         private String processRequest(String request) {
-            // Perform basic math calculations
-            // Addition, Subtraction, Multiplication, Division
-        	// Split the request into operands and operator
+            // Perform Addition, Subtraction, Multiplication, Division
+        	// Split the request 
             String[] parts = request.split("\\s+");
+            // User must enter 3 parts only
             if (parts.length != 3) {
                 return "Invalid request format. Please use 'operand operator operand'.";
             }
 
             try {
+            	// Initialize parts 1, 2, and 3
                 int operand1 = Integer.parseInt(parts[0]);
                 String operator = parts[1];
                 int operand2 = Integer.parseInt(parts[2]);
 
+                // Perform math based on operator
                 int result;
                 switch (operator) {
                     case "+":
@@ -87,19 +98,21 @@ public class Server {
                         result = operand1 * operand2;
                         break;
                     case "/":
+                    	// Error if divide by Zero
                         if (operand2 == 0) {
                             return "Cannot divide by zero.";
                         }
                         result = operand1 / operand2;
                         break;
                     default:
+                    	// If operator is invalid for the program
                         return "Invalid operator. Please use '+', '-', '*', or '/'.";
                 }
+                // Return result of computation
                 return String.valueOf(result);
             } catch (NumberFormatException e) {
                 return "Invalid operands. Please use numbers for operands.";
             }
-            //return "Processed: " + request;
         }
     }
 }
